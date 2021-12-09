43432e360aaa3e520ef2bac8bb000a0350d6f369
Tushar <chauhantushar282@gmail.com>
2021-12-09 13:40:29 +0530

    a

---
+++ js/custom.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+	//$("#tushar").click(function(){
+		//$("#hello").addClass("cuha");
+		//$("#hello").removeClass("cuha");
+		//$("#ch").toggle();
+		//$("#ch").hide();
+		
+		//$("#ch").show();	
+		 //$("#tllu").animate({left:'250px'});
+	
+		//});
+			//$(".navbar-nav li").each(function(){
+			//$(this).click(function(){
+			//$(this).addClass("bhu");
+			//$(this).siblings().removeClass("bhu");
+			//});
+				
+			//});
+		//$(".rr6").parents().click(function(){
+			//$(".rr5").addClass("bhu");
+		//});
+		$(".rr5").children().click(function(){
+			$(".rr5").addClass("bhu");
+		});
+		console.log('heading');
+		//$("#tillu").dblclick(function(){
+		//var heading =  "Business Solutions";
+		//var name="Tushar"
+		//$("#hello").html(heading);
+		//$("#ch").html(name);
+		
+	
+	
+	//});
+	//$("#ch").mousedown(function(){
+		
+		//$("#ch").hide();
+	//});
+	//$("#hello").mouseenter(function(){
+		
+		//$("#ch").hide();
+	//});
+});
+	
